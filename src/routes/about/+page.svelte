ae064da634417bf8ded65a73eb513838d896c993
josefineschaefer <josefine.schaefer@storyblok.com>
2023-01-09 11:14:14 +0100

    about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,23 +1,18 @@
-<body class="h-full p-16">
-	<main class="mt-24">
-		<h2 class="text-left">Community, Code & Coffee</h2>
-		<p class="pb-10 text-xl indent-96">
+<main class="p-28">
+	<h1 class="pb-10">ABOUT &emsp ME</h1>
+	<section class="grid md:grid-cols-2 gap-12">
+		<img
+			alt="portrait of Josefine"
+			src="/images/JosefinePortrait.png"
+			class="max-w-xs h-auto rounded-lg mr-5"
+		/>
+		<p class="max-w-md text-xl pb-10">
 			Initially from a communications background, Josefine got into frontend development in her late
 			twenties by joining a coding boot camp. After working as a front-end developer for two years,
 			she now combines her curiosity for tech with her experience in marketing and communications
 			and works as a Developer Relations Engineer at Storyblok. She is a Girl Code ambassador,
 			passionate about all things web accessibility and coding communities.
 		</p>
-		<section class="grid justify-beteween">
-			<img
-				alt="portrait of Josefine"
-				src="/images/JosefinePortrait.png"
-				class="max-w-xs h-auto rounded-lg mr-5"
-			/>
-
-			<h1 class="text-right self-end break-normal">
-				Josefine <br /> Schaefer
-			</h1>
-		</section>
-	</main>
-</body>
+	</section>
+	<h2 class="text-right">get in touch</h2>
+</main>
