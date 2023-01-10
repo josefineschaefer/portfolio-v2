1f8cf7e809ad4b0ad4d203b41c057f76b7e9cd7a
josefineschaefer <josefine.schaefer@storyblok.com>
2023-01-10 10:11:09 +0100

    adding talk content

--- src/routes/talks/+page.svelte
+++ src/routes/talks/+page.svelte
@@ -36,9 +36,14 @@
 				/></svg
 			>
 		</span>
-		<p class="mb-2 text-gray-500 dark:text-gray-400">
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ab necessitatibus sint
-			explicabo ...
+		<p class="mb-2 text-xl dark:text-gray-400">
+			Accessibility is a decisive tool to make the internet a more inclusive place, it is absolutely
+			crucial and despite the popular belief that only a small user group is affected, it benefits
+			every single one of us. The reality doesn’t yet reflect that, though - accessibility often
+			just seems like an afterthought. I will show the benefits of implementing accessibility and
+			eliminate some of the prejudices. ​I want the audience to take away the fact that it's rather
+			easy to get started, that a little bit is better than nothing and give them helpful tools to
+			put their motivation into action.
 		</p>
 	</AccordionItem>
 	<AccordionItem>
@@ -73,9 +78,12 @@
 				/></svg
 			>
 		</span>
-		<p class="mb-2 text-gray-500 dark:text-gray-400">
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ab necessitatibus sint
-			explicabo ...
+		<p class="mb-2 text-xl dark:text-gray-400">
+			You want to go beyond roughly meeting guidelines but actually create products that cater a
+			large audience and their needs? To not only fulfil requirements but actually create mindful
+			content & empathic code? Anybody working in tech has the responsibility to provide an
+			inclusive, accessible experience. Let's dive into web accessibility best practices, testing &
+			auditing strategies and well-guarded WCAG secrets to take your project to the next level.
 		</p>
 	</AccordionItem>
 	<AccordionItem>
@@ -110,9 +118,14 @@
 				/></svg
 			>
 		</span>
-		<p class="mb-2 text-gray-500 dark:text-gray-400">
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ab necessitatibus sint
-			explicabo ...
+		<p class="mb-2 text-xl dark:text-gray-400">
+			"Imposter Syndrome" is a buzzword in the tech industry; it seems to be ingrained in being a
+			developer. Have we all just accepted this dreadful notion that we don’t really belong and will
+			be found out in the long run? We need a more solution-oriented, inclusive approach to dealing
+			with our insecurities. We need to collectively learn how to communicate and deal with them to
+			make all our lives, but especially those who are just getting started in their careers,
+			easier. That’s why I am sharing hands strategies on how to deal with imposter syndrome at
+			work.
 		</p>
 	</AccordionItem>
 	<AccordionItem>
@@ -147,9 +160,12 @@
 				/></svg
 			>
 		</span>
-		<p class="mb-2 text-gray-500 dark:text-gray-400">
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ab necessitatibus sint
-			explicabo ...
+		<p class="mb-2 text-xl dark:text-gray-400">
+			Svelte has been described as an “accessibility-first-framework”, yet its compiler warnings
+			cover only a fragment of the issues at hand. In this talk, I’d like to explore how accessible
+			Svelte really is and more importantly, have a look at what you can do to bridge the gap. Ideas
+			include checking both automatically and manually for issues the compiler might overlook, like
+			color contrast, keyboard accessibility and semantics.
 		</p>
 	</AccordionItem>
 	<AccordionItem>
@@ -184,9 +200,18 @@
 				/></svg
 			>
 		</span>
-		<p class="mb-2 text-gray-500 dark:text-gray-400">
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ab necessitatibus sint
-			explicabo ...
+		<p class="mb-2 text-xl dark:text-gray-400">
+			You want to take your web development game to the next level? Be an outstanding asset to your
+			team? Maybe even lead and mentor others? Instead of IDE shortcuts, productivity hacks and new
+			shiny frameworks, let’s talk about allyship. As communities and teams, we must support each
+			other to reach our common goal. Studies show that a wide range of different folks on a team
+			will make the product or service better, improve code quality & make progress more
+			sustainable. But one (big) step towards more diverse (and humane) teams is allyship - from the
+			recruiting process to team culture & inclusive language. We’ll have a closer look at what it
+			means to be an ally, why it’s crucial and what concrete steps you can take. It’s going to be a
+			wild ride. It might hurt a little. But it’s going to make things better in the long run. From
+			this talk, you will be able to take away concrete steps you can apply in your day-to-day life
+			- making the tech community more inclusive, one step at a time.
 		</p>
 	</AccordionItem>
 	<AccordionItem>
@@ -221,9 +246,13 @@
 				/></svg
 			>
 		</span>
-		<p class="mb-2 text-gray-500 dark:text-gray-400">
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ab necessitatibus sint
-			explicabo ...
+		<p class="mb-2 text-xl dark:text-gray-400">
+			Developer Experience (DX) has become somewhat of a buzzword lately. But what is this trend
+			really all about? Think about your experience with Svelte & Sveltekit. How things 'just work',
+			and (for the most part) so quickly. In short: good DX feels like magic. Let's explore
+			practical ways to create your tools with the best possible DX by having a look at real-world
+			examples and see what we can do to improve developer experience in the Svelte community and
+			beyond.
 		</p>
 	</AccordionItem>
 </Accordion>
